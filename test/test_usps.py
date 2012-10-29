e1a2be11ce5c562c90cefaf0f5157eeb21bdfbb7
Thomas Levine <occurrence@thomaslevine.com>
2012-10-29 13:46:11 -0400

    another test of the same thing

--- test/test_usps.py
+++ test/test_usps.py
@@ -13,14 +13,14 @@ class TestParse:
 
     def test_multiple_results(self):
         self._('ZipLookupResultsAction!input.action?city=Washington&zip=20500&companyName=&address1=1600+Pennsylvania+Avenue+NW&address2=&postalCode=&state=DC&resultMode=0&urbanCode=', [
-            {u"city": u"WASHINGTON", u"state": u"DC", u"address1": u"1600 PENNSYLVANIA AVE NW", u"zip": u"20500", u"zip4": u"0003"}, 
-            {u"city": u"WASHINGTON", u"name": u"WHITE HOUSE", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0003"}, 
-            {u"city": u"WASHINGTON", u"name": u"PRESIDENT", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0001"}, 
-            {u"city": u"WASHINGTON", u"name": u"FIRST LADY", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0002"}, 
-            {u"city": u"WASHINGTON", u"name": u"THE WHITE HOUSE", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0004"}, 
-            {u"city": u"WASHINGTON", u"state": u"DC", u"address1": u"1600 PENNSYLVANIA AVE NW", u"zip": u"20500", u"zip4": u"0005"}, 
-            {u"city": u"WASHINGTON", u"name": u"GREETINGS OFFICE", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0039"}, 
-            {u"city": u"WASHINGTON", u"name": u"WHITE HOUSE STATION", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0049"}, 
+            {u"city": u"WASHINGTON", u"state": u"DC", u"address1": u"1600 PENNSYLVANIA AVE NW", u"zip": u"20500", u"zip4": u"0003"},
+            {u"city": u"WASHINGTON", u"name": u"WHITE HOUSE", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0003"},
+            {u"city": u"WASHINGTON", u"name": u"PRESIDENT", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0001"},
+            {u"city": u"WASHINGTON", u"name": u"FIRST LADY", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0002"},
+            {u"city": u"WASHINGTON", u"name": u"THE WHITE HOUSE", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0004"},
+            {u"city": u"WASHINGTON", u"state": u"DC", u"address1": u"1600 PENNSYLVANIA AVE NW", u"zip": u"20500", u"zip4": u"0005"},
+            {u"city": u"WASHINGTON", u"name": u"GREETINGS OFFICE", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0039"},
+            {u"city": u"WASHINGTON", u"name": u"WHITE HOUSE STATION", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"0049"},
             {u"city": u"WASHINGTON", u"name": u"AMER FUND FOR AFGHAN CHILD", u"zip": u"20500", u"address1": u"1600 PENNSYLVANIA AVE NW", "state": u"DC", u"zip4": u"1600"},
         ])
 
@@ -41,6 +41,9 @@ class TestParse:
 
     def test_not_recognized(self):
         self._('ZipLookupResultsAction!input.action?resultMode=0&companyName=&address1=26130+Birch+AVE&address2=&city=Ni%0Asswa&state=MN&urbanCode=&postalCode=&zip=56468', [])
+
+    def test_not_found2(self):
+        self._('ZipLookupResultsAction!input.action?city=None&state=MN&postalCode=&urbanCode=&companyName=&resultMode=0&address1=1972+Timber+Wolf+TRL+S&address2=&zipcode=55122', [])
 
 
 def test_white_house():
